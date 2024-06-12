342ea615ff992659300730e7252dcc62e6bc7ec7
Luke Evans <sir.lukeevans@gmail.com>
2024-06-12 14:38:05 -0400

    Basic gameManager creation and first TestScene setup

---
+++ FSU-Project/Assets/Scripts/LukeScripts/gameManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class gameManager : MonoBehaviour
+{
+    public static gameManager instance;
+
+    [SerializeField] NavMeshAgent agent;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
