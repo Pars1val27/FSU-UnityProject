9629382664653ccc36d3cd1adb6a417b1498247e
Jasen Blevins <135083567+JBlevins52736@users.noreply.github.com>
2024-06-12 21:06:39 -0400

    final grenade changes  and prefab correction

--- FSU-Project/Assets/Scripts/JasenScripts/Grenade.cs
+++ FSU-Project/Assets/Scripts/JasenScripts/Grenade.cs
@@ -4,64 +4,65 @@ using UnityEngine;
 
 public class Grenade : MonoBehaviour
 {
-    
-        [SerializeField] private float delay = 3f;
-        [SerializeField] private float explosionRadius = 5f;
-        [SerializeField] private float explosionForce = 700f;
-        [SerializeField] private int damage = 5;
-        [SerializeField] private ParticleSystem explosionEffect;
+    [SerializeField] int delay;
+    [SerializeField] float explosionRadius = 5;
+    [SerializeField] float explosionForce = 50f;
+    [SerializeField] int damage = 5;
+    [SerializeField] ParticleSystem explosionEffect;
 
-        private bool hasExploded = false;
-        private float countdown;
+    bool hasExploded = false;
+    float countdown;
 
-        void Start()
+    void Start()
+    {
+        countdown = delay;
+    }
+
+    void Update()
+    {
+        countdown -= Time.deltaTime;
+        if (countdown <= 0f && !hasExploded)
         {
-            countdown = delay;
+            Explode();
+            hasExploded = true;
         }
+    }
 
-        void Update()
+    void Explode()
+    {
+        
+        ParticleSystem effect = Instantiate(explosionEffect, transform.position, transform.rotation);
+        effect.Play();
+
+        Collider[] colliders = Physics.OverlapSphere(transform.position, explosionRadius);
+        foreach (Collider nearbyObject in colliders)
         {
-            countdown -= Time.deltaTime;
-            if (countdown <= 0f && !hasExploded)
+           
+            Rigidbody rb = nearbyObject.GetComponent<Rigidbody>();
+            if (rb != null)
             {
-                Explode();
-                hasExploded = true;
+                rb.AddExplosionForce(explosionForce, transform.position, explosionRadius);
             }
-        }
-
-        void Explode()
-        {
-          
-            explosionEffect.transform.parent = null; 
-            explosionEffect.transform.position = transform.position;
-            explosionEffect.Play();
 
            
-            Collider[] colliders = Physics.OverlapSphere(transform.position, explosionRadius);
-            foreach (Collider nearbyObject in colliders)
+            IDamage dmg = nearbyObject.GetComponent<IDamage>();
+            if (dmg != null)
             {
-                Rigidbody rb = nearbyObject.GetComponent<Rigidbody>();
-                if (rb != null)
-                {
-                    rb.AddExplosionForce(explosionForce, transform.position, explosionRadius);
-                }
-
-                IDamage dmg = nearbyObject.GetComponent<IDamage>();
-                if (dmg != null)
-                {
-                    dmg.TakeDamage(damage);
-                }
+                dmg.TakeDamage(damage);
             }
-
-            
-            Destroy(gameObject);
         }
 
-        void OnDrawGizmosSelected()
-        {
-            
-            Gizmos.color = Color.red;
-            Gizmos.DrawWireSphere(transform.position, explosionRadius);
-        }
-   }
+        
+        Destroy(effect.gameObject, effect.main.duration);
 
+        
+        Destroy(gameObject);
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, explosionRadius);
+    }
+}
