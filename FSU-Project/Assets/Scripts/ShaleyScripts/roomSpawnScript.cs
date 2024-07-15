4fc5722f638f0f105397e3afe8ce817c16de44d8
ShaleyB <155989177+ShaleyB@users.noreply.github.com>
2024-07-14 22:22:30 -0400

    Working timer when player enters hall

--- FSU-Project/Assets/Scripts/ShaleyScripts/roomSpawnScript.cs
+++ FSU-Project/Assets/Scripts/ShaleyScripts/roomSpawnScript.cs
@@ -1,14 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class roomSpawnScript : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("player"))
+        if (other.CompareTag("Player"))
         {
-            //Timer.timerInstance.StartTimer();
+            Clock.timeInstance.StartTimer();
         }
     }
 }
