5f2c1e79e7f935eb633c20d9c28c225097b83693
Luke evans <sir.lukeevans@gmail.com>
2024-07-21 18:52:46 -0400

    Status Effect implementation

--- FSU-Project/Assets/Scripts/LukeScripts/turretDynamic.cs
+++ FSU-Project/Assets/Scripts/LukeScripts/turretDynamic.cs
@@ -18,7 +18,7 @@ public class turretDynamic : MonoBehaviour, IDamage
 
     [Header("----- Animation's -----")]
     [SerializeField] Animator anim;
-    [SerializeField] Renderer[] model;
+    [SerializeField] Renderer model;
     [SerializeField] GameObject swivel;
     [SerializeField] ParticleSystem spark;
     [SerializeField] ParticleSystem spawnEffect;
@@ -35,6 +35,10 @@ public class turretDynamic : MonoBehaviour, IDamage
 
     bool isshooting;
     float StartHP;
+    bool isSlowed = false;
+    bool isFrozen = false;
+    float originalSpeed;
+    float origAttackRate;
 
     void Start()
     {
@@ -45,17 +49,136 @@ public class turretDynamic : MonoBehaviour, IDamage
 
     private void Update()
     {
-        Quaternion rot = Quaternion.LookRotation(-new Vector3(playerDir.x, 0, playerDir.z));
-        healthBar.transform.rotation = rot;
-        playerPos = EnemyManager.instance.player.transform.position;
-        playerDir = playerPos - transform.position;
-        faceTarget();
-        if (!isshooting)
+        if (!isFrozen)
         {
-            StartCoroutine(shoot());
+            Quaternion rot = Quaternion.LookRotation(-new Vector3(playerDir.x, 0, playerDir.z));
+            healthBar.transform.rotation = rot;
+            playerPos = EnemyManager.instance.player.transform.position;
+            playerDir = playerPos - transform.position;
+            faceTarget();
+            if (!isshooting)
+            {
+                StartCoroutine(shoot());
+            }
         }
-        
     }
+
+    // Status Effect  Implementaion
+    public void ApplyFireDamage(int fireDamage, float duration, GameObject fireEffect)
+    {
+        Debug.Log("brute current Hp" + HP);
+        GameObject FireEffect = Instantiate(fireEffect, transform.position, Quaternion.identity, transform);
+        FireEffect.transform.SetParent(transform);
+        StartCoroutine(FireDamageCoroutine(fireDamage, duration, FireEffect));
+    }
+
+    IEnumerator FireDamageCoroutine(int fireDamage, float duration, GameObject FireEffect)
+    {
+        float elapsed = 0f;
+        while (elapsed < duration)
+        {
+            Debug.Log("brute Hp" + HP);
+            TakeDamage(fireDamage);
+            elapsed += 1f;
+            Debug.Log(fireDamage + " FireDamage");
+            yield return new WaitForSeconds(1f);
+
+        }
+        Destroy(FireEffect);
+    }
+    public void ApplyPoisonDamage(int PosionDamage, float duration, GameObject poisonEffect)
+    {
+        GameObject PoisonEffect = Instantiate(poisonEffect, transform.position, Quaternion.identity, transform);
+        PoisonEffect.transform.SetParent(transform);
+        StartCoroutine(PoisonDamageCoroutine(PosionDamage, duration, PoisonEffect));
+    }
+
+
+    private IEnumerator PoisonDamageCoroutine(int PosionDamage, float duration, GameObject PoisonEffect)
+    {
+        float elapsed = 0f;
+        while (elapsed < duration)
+        {
+            Debug.Log("brute Hp" + HP);
+
+            TakeDamage(PosionDamage);
+            elapsed += 1f;
+            Debug.Log(PosionDamage + " PoisonDamage");
+            yield return new WaitForSeconds(1f);
+
+        }
+        Destroy(PoisonEffect);
+    }
+
+
+
+    public void ApplySlow(float slowAmount, float duration, GameObject slowEffect)
+    {
+        if (!isSlowed)
+        {
+            isSlowed = true;
+
+            Debug.Log(agent.speed + " normal speed");
+
+            agent.speed = slowAmount;
+            shootRate = slowAmount;
+            Debug.Log(agent.speed + " Slow Speed Slow Start ");
+
+            GameObject SlowEffect = Instantiate(slowEffect, transform.position, Quaternion.identity, transform);
+            SlowEffect.transform.SetParent(transform);
+            SlowEffect.transform.localScale = Vector3.one;
+            StartCoroutine(SlowCoroutine(duration, SlowEffect));
+        }
+    }
+    private IEnumerator SlowCoroutine(float duration, GameObject SlowEffect)
+    {
+
+        yield return new WaitForSeconds(duration);
+        RemoveSlow();
+        Destroy(SlowEffect);
+    }
+    public void RemoveSlow()
+    {
+        isSlowed = false;
+
+        agent.speed = originalSpeed;
+        shootRate = origAttackRate;
+
+        Debug.Log(agent.speed + " Slow end normal speed");
+
+    }
+
+
+
+
+    public void ApplyFreeze(float duration, GameObject freezeEffect)
+    {
+        if (!isFrozen)
+        {
+            Debug.Log(gameObject.name + " Enemy Frozen");
+            isFrozen = true;
+            GameObject FreezeEffect = Instantiate(freezeEffect, transform.position, Quaternion.identity, transform);
+            FreezeEffect.transform.SetParent(transform);
+            StartCoroutine(FreezeCoroutine(duration, FreezeEffect));
+        }
+    }
+
+    private IEnumerator FreezeCoroutine(float duration, GameObject FreeezEffect)
+    {
+
+        agent.speed = 0f;
+        anim.enabled = false;
+
+        yield return new WaitForSeconds(duration);
+        anim.enabled = true;
+        agent.speed = originalSpeed;
+        model.material.color = Color.white;
+        Debug.Log(gameObject.name + " Enemy Unfrozen");
+        Destroy(FreeezEffect);
+        isFrozen = false;
+    }
+
+    //Status Effect Implementation end
 
     void faceTarget()
     {
